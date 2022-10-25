c70b3ca3b8128e791e3915b6b48114e48460b9ab
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2022-10-24 21:40:27 -0300

    [REFACTOR] Main

--- src/main/java/clases/Main.java
+++ src/main/java/clases/Main.java
@@ -1,8 +1,57 @@
 package clases;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola");
+    public static void main(String[] args) throws InterruptedException {
+        //Menú
+        System.out.println(" ");
+        System.out.println("                    Bienvenido a Fast&will - Software de gestión de concesionarias");
+        System.out.println("*****************************************************************************************************");
+//        System.out.println("");
+//        System.out.println("Elija una opción para operar: ");
+//        System.out.println("------------------------------");
+//        System.out.println("Opción 0: Ventas");
+//        System.out.println("Opción 1: Buscar");
+//        System.out.println("Opción 2: Servicios de alquiler");
+//        System.out.println("Opción 3: Carga de datos");
+//        System.out.println("Opción 4: Compras");
+//        System.out.println("Opción 5: Salír");
+//        System.out.println("");
+
+//auto
+        Thread.sleep(1*1000);
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXO------------OXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        Thread.sleep(200);
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|            |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        Thread.sleep(200);
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|Team pro_Utn|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        Thread.sleep(200);
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX|            |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        Thread.sleep(200);
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXO------------OXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        System.out.println("                                             _._");
+        System.out.println("                                        _.-=´´_-         _");
+        System.out.println("                                   _.-=´´   _-          | |´´´´´´´---._______     __..");
+        System.out.println("                       ___.===¨¨¨¨-.______-,,,,,,,,,,,,`-¨ ¨----´¨¨ ¨¨¨¨       ¨¨¨¨  __¨¨");
+        System.out.println("                _.--¨¨¨     _        ,´                   o \\           _        [_]|");
+        System.out.println("           __-´´=======.--¨¨  ¨¨--.=================================.--¨¨  ¨¨--.=======:");
+        System.out.println("          ]       [w] : /        \\ : |========================|    : /        \\ :  [w] :");
+        System.out.println("          V___________:|          |: |========================|    :|          |:   _-´");
+        System.out.println("           V__________: \\        / :_|=======================/_____: \\        / :__-´");
+        System.out.println("           -----------´  ¨¨____¨¨  `-------------------------------´  ¨¨____¨¨");
+
+
+        System.out.println("         OPCIONES DE VENTA");
+        System.out.println("___________________________________");
+        System.out.println("0-Vehículo por marca.");
+        System.out.println("1-Planes de pago.");
+        System.out.println("2-Venta Auto.");
+        System.out.println("3-Venta Repuesto.");
+        System.out.println("4-Salir.");
+
+        System.out.print("Ingrese opcion: ");
+
     }
+
+
 
 }
