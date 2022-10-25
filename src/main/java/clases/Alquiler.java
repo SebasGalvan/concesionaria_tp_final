13c194d4ff4171ae8b23dabbe8a0b5102598b1a1
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2022-10-24 22:47:52 -0300

    [REF] Alquiler

--- src/main/java/clases/Alquiler.java
+++ src/main/java/clases/Alquiler.java
@@ -2,8 +2,9 @@ package clases;
 
 import java.util.Date;
 
-public class Alquiler extends Operaciones{
+public class Alquiler extends Operaciones {
 
     public Alquiler(int legajo, String dni, String nombre, String apellido, Date fecha, Long idAuto, Long idPlan) {
         super(legajo, dni, nombre, apellido, fecha, idAuto, idPlan);
+    }
 }
