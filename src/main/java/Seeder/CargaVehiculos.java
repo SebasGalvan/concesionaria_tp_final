4a4ab48e38dc2d293fb1a6744c1cecafbb9be269
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2022-10-31 18:21:37 -0300

    [UPDATE] Seeder carga de Vehiculos limpio

--- src/main/java/Seeder/CargaVehiculos.java
+++ src/main/java/Seeder/CargaVehiculos.java
@@ -3,18 +3,8 @@ package Seeder;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class CargaVehiculo {
+public class CargaVehiculos {
 
-    public static void insertarPais(PreparedStatement stmt) {
-
-        try {
-            String SQL = "INSERT INTO paises (pais, acronimo, poblacion) VALUES ('Argentina', 'AR', 51000000)";
-            int count = stmt.executeUpdate(SQL);
-            System.out.println("filas afectadas: " + count);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
 
 }
