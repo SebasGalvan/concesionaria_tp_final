4cae8c3d23b6903c20dcce8eafaee1662249befc
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2022-10-31 18:21:07 -0300

    [UPDATE] Operaciones sobre BBDD

--- src/main/java/DataBase/DDLOperations.java
+++ src/main/java/DataBase/DDLOperations.java
@@ -1,37 +1,39 @@
-package BaseDeDatos;
+package DataBase;
 
-import Seeder.CreacionDeTablas;
+import Configuration.PropertiesFile;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
-public class DB_Connect {
+public class DDLOperations {
+        public static void createDB() {
 
-    private final static String base = "libreria";
-
-    public DB_Connect() {
-
-    }
-    public static void createDB() {
-        try(Connection cn =  new Conexion().getConexion();) {
+        Connection cn = null;
+        try{
+            Class.forName(Conexion.driver);
+            cn = DriverManager.getConnection(Conexion.url, Conexion.user, Conexion.password);
             Statement stmt = cn.createStatement();
-            stmt.execute("CREATE DATABASE IF NOT EXISTS " + base);
+            stmt.execute("CREATE DATABASE IF NOT EXISTS " + Conexion.database);
             System.out.println("Base de datos creada correctamente");
             stmt.close();
-
+            cn.close();
         } catch (SQLException e) {
             System.out.println("Error en la conexión:" + e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public static void operationsDB(String OP) {
         try(Connection cn =  new Conexion().getConexion()) {
             Statement stmt = cn.createStatement();
-            Boolean a = stmt.execute(OP + " DATABASE " + base);
+            Boolean a = stmt.execute(OP + " DATABASE " + Conexion.database);
             System.out.println("Operacion Realizada...");
             stmt.close();
-
+            cn.close();
         } catch (SQLException e) {
             System.out.println("Error en la conexión:" + e);
         }
